5b786857abf6657b8c5a2808cab490a84634acb9
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-07-25 11:45:22 -0400

    Added images

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const Characters = {
       "Shy, timid, cowardly, brave (at times), friendly, nervous, anxious, bashful, loyal, nerdy, smart, stubborn",
     Quote: "I don't think this is such a good idea.",
     Enemies: "Angelica",
-    image: "Chuckie.webp",
+    image: "chuckiefinster-rugrats-character.webp",
   },
   phildeVille: {
     fullName: "Phillip Richard William DeVille",
