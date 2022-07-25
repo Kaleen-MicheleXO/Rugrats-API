92cf8e8cdcc60b3e24bf190b11f486d880bb2f5a
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-07-25 11:04:37 -0400

    Added image

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const Characters = {
     Personality: "Fearless, happy-go-lucky, brave, smart, kind",
     Quote: "A baby's gotta do what a baby's gotta do!",
     Enemies: "Angelica",
+    image: "Tommy-Pickles-from-Rugrats.webp",
   },
   chuckiefinster: {
     fullName: "Charles Crandall Finster Jr. (III)",
